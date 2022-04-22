b1f318ac53d3f98fc759948324995002b9c7ae75
Thuy NT <thuthuy.nguyen1509@gmail.com>
2022-04-22 13:32:33 +0700

    update

--- webpack.mix.js
+++ webpack.mix.js
@@ -52,6 +52,7 @@ mix
     })
     .copy('./src/app.html', './docs/index.html')
     .copy('./src/longform-nn6.html', './docs/longform-nn6.html')
+    .copy('./src/longform-nn6-new.html', './docs/longform-nn6-new.html')
     .copy('./src/video.html', './docs/')
     .copy('./src/longform-xithong.html', './docs/')
     .copyDirectory('./src/js','./docs/js')
